d76083b3cf5e408d6438a781d422a1d4c71c25ad
cscmx <sainz.carolina@gmail.com>
2026-06-03 23:14:52 +0200

    Código extra en ejercicio6 porque tenía duda de que estuviera bien hecho el 50% de probabilidad

--- ejercicio6.php
+++ ejercicio6.php
@@ -2,14 +2,23 @@
 
 function isBitten() : string {
 
-    if (rand(0,100) <= 50){
+    if (rand(1,100) <= 50){
         return "TRUE"; 
     } else {
         return "FALSE";
     }
 
 }
-
 echo isBitten();
+
+
+/*código para verificar el 50% de posibilidades - porque me entró la duda*/
+$contador = 0;
+for ($num = 0; $num<100000; $num++){
+    if (isBitten()=="TRUE"){
+        $contador++;
+    }
+}
+echo "contador = ".$contador;
 
 ?>
