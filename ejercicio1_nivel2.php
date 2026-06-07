c45d4c3f912c593b36a3889f3325856c3839a40a
cscmx <sainz.carolina@gmail.com>
2026-06-07 17:36:28 +0200

    Changed the function names from spanish to english

--- ejercicio1_nivel2.php
+++ ejercicio1_nivel2.php
@@ -4,22 +4,21 @@
 Tota trucada que duri menys de 3 minuts té un cost de 10 cèntims.
 Cada minut addicional a partir dels 3 primers és un pas de comptador i costa 5 cèntims.
 */
-
-function calcularTiempoExtra (int $tiempoExtra) {
+function calculateExtraTime (int $tiempoExtra) {
 
     return $tiempoExtra * 5;
 }
 
-function calculoPrecioLlamada (int $tiempoLlamada)  {
+function calculateCallPrice (int $tiempoLlamada)  {
     
     if ($tiempoLlamada < 3) {
         return 10;
     } else {
         $tiempoLlamada = $tiempoLlamada - 3;
-        $totalExtra = calcularTiempoExtra($tiempoLlamada);
+        $totalExtra = calculateExtraTime($tiempoLlamada);
         return 10 + $totalExtra;
     }
 
 }
 
-echo calculoPrecioLlamada(10);
+echo "The total price of the call is: ".calculateCallPrice(10);
