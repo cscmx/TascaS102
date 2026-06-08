cac20a17892f63fd30c8d03661e4ea047c83078a
cscmx <sainz.carolina@gmail.com>
2026-06-08 22:07:55 +0200

    Update ejercicio5

--- ejercicio5.php
+++ ejercicio5.php
@@ -1,12 +1,22 @@
 <?php
+/*
+Escriure una funció per verificar el grau d'un/a estudiant d'acord amb la nota.
+
+Condicions:
+
+Si la nota és 60% o més, el grau hauria de ser Primera Divisió.
+Si la nota està entre 45% i 59%, el grau hauria de ser Segona Divisió.
+Si la nota està entre 33% to 44%, el grau hauria de ser Tercera Divisió.
+Si la nota és menor a 33%, l'estudiant reprovarà.
+*/
 
 function verifyingGrade (int $note) : string {
     
     if ($note>=60) {
         return "Primera División";
-    } else if ($note >= 45 && $note <= 59){
+    } else if ($note >= 45){
         return "Segunda División"; 
-    } else if ($note >= 33 && $note <= 44){
+    } else if ($note >= 33){
         return "Tercera División";
     } else {
         return "No aprobado";
@@ -14,7 +24,7 @@ function verifyingGrade (int $note) : string {
 
 }
 
-$notetoEvaluate = verifyingGrade(15);
+$notetoEvaluate = verifyingGrade(65);
 
 echo "El grado del alumno es: ".$notetoEvaluate;
 
