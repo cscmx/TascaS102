acb62920b3da6834154d4129f6258b65a9cac688
cscmx <sainz.carolina@gmail.com>
2026-06-08 22:11:17 +0200

    update ejercicio1 nivel1

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,13 +1,19 @@
 <?php
+/*
+Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla/terminal.
+
+Després crea una constant que inclogui el teu nom i mostra-ho en format títol per pantalla o imprimeix-la per terminal.
+*/
+
     $varNum = 2;
     $varDouble = 2.3;
     $varString = "hola";
     $varBool = true;
 
-    echo $varNum .'<br>';
-    echo $varDouble .'<br>';
-    echo $varString .'<br>';
-    echo $varBool .'<br>';
+    echo $varNum .PHP_EOL;
+    echo $varDouble .PHP_EOL;
+    echo $varString .PHP_EOL;
+    echo $varBool .PHP_EOL;
 
     define ('NOMBRE', "Carolina Sainz");
     echo  NOMBRE ;
