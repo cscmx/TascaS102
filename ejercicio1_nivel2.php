520fa9fe8d49dd0c953a9f79d5feb28604bb5f55
cscmx <sainz.carolina@gmail.com>
2026-06-08 22:16:57 +0200

    Update ejercicio1 NIVEL2

--- ejercicio1_nivel2.php
+++ ejercicio1_nivel2.php
@@ -4,21 +4,20 @@
 Tota trucada que duri menys de 3 minuts té un cost de 10 cèntims.
 Cada minut addicional a partir dels 3 primers és un pas de comptador i costa 5 cèntims.
 */
-function calculateExtraTime (int $tiempoExtra) {
 
-    return $tiempoExtra * 5;
-}
+define ('EXTRAMINUTE', 5);
+define ('FIRST3MINUTES', 10);
+
 
 function calculateCallPrice (int $tiempoLlamada)  {
     
     if ($tiempoLlamada < 3) {
-        return 10;
+        return FIRST3MINUTES;
     } else {
         $tiempoLlamada = $tiempoLlamada - 3;
-        $totalExtra = calculateExtraTime($tiempoLlamada);
-        return 10 + $totalExtra;
+        $totalExtra = $tiempoLlamada * EXTRAMINUTE;
+        return FIRST3MINUTES + $totalExtra;
     }
-
 }
 
 echo "The total price of the call is: ".calculateCallPrice(10);
